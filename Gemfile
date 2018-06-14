f16a191de835430646bbb264374bc6b8535857db
Mateus Luiz <mateuslfreitasb@gmail.com>
2018-06-14 10:56:45 -0300

    Setup VCR and Webmock

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
   gem "factory_bot_rails"
   gem "rubocop-rails"
+  gem "webmock"
 end
 
 group :development do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,16 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'webmock/rspec'
+require "vcr"
+
+WebMock.disable_net_connect!(allow_localhost: true)
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/vcr_cassetes"
+  config.hook_into :webmock
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
