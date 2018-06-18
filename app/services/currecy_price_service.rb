bd82ad086fe491e62d0247c6664169076d557251
Mateus Luiz <mateuslfreitasb@gmail.com>
2018-06-18 16:04:20 -0300

    Add test to "coin_price" method from CurrecyPriceService

--- app/services/currecy_price_service.rb
+++ app/services/currecy_price_service.rb
@@ -1,3 +1,8 @@
 class CurrecyPriceService
   include Singleton
+
+  def coin_price
+    url = "#{ENV['CRYPTO_COMPARE_API_URL']}/data/price?fsym=BTC&tsyms=USD"
+    HTTParty.get(url, headers: {"Content-Type" => "application/json"})
+  end
 end
--- spec/services/currecy_price_service_spec.rb
+++ spec/services/currecy_price_service_spec.rb
@@ -3,7 +3,18 @@ require "rails_helper"
 RSpec.describe CurrecyPriceService do
   describe "initialization" do
     it "can instantiate the singleton service" do
-      expect(CurrecyPriceService.instance).to be_a CurrecyPriceService
+      expect(described_class.instance).to be_a CurrecyPriceService
+    end
+  end
+
+  describe "#coin_price" do
+    let(:url) { "#{ENV['CRYPTO_COMPARE_API_URL']}/data/price?fsym=BTC&tsyms=USD" }
+    let(:headers) { { "Content-Type" => "application/json" } }
+    subject(:coin_price) { described_class.instance.coin_price }
+
+    it "should call the cryptocompare API" do
+      expect(HTTParty).to receive(:get).with(url, headers: headers)
+      coin_price
     end
   end
 end
