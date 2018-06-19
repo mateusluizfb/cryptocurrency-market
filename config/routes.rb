d4cf41d8eba00160e634cf2d76250f5648bee52f
Mateus Luiz <mateuslfreitasb@gmail.com>
2018-06-18 22:28:01 -0300

    Add coins prices listing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "crypto_currencies#prices"
 
   get :prices, to: "crypto_currencies#prices"
 end
