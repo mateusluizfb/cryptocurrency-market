f904308f583a19be95dc340c7154d062fa6ee410
Mateus Luiz <mateuslfreitasb@gmail.com>
2018-06-19 19:35:57 -0300

    Implement the basic "new" action from OrdersController

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "crypto_currencies#prices"
 
-  get :prices, to: "crypto_currencies#prices"
+  get "/crypto_currencies/prices", to: "crypto_currencies#prices"
+  get "/orders/new", to: "orders#new"
 end
