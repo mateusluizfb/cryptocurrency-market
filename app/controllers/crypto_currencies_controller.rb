c154757f7c02d60d3e8e449cca689da70eec6ab0
Mateus Luiz <mateuslfreitasb@gmail.com>
2018-06-18 22:17:38 -0300

    Implement prices action logic

--- app/controllers/crypto_currencies_controller.rb
+++ app/controllers/crypto_currencies_controller.rb
@@ -1,4 +1,7 @@
 class CryptoCurrenciesController < ApplicationController
+  COINS = %w[BTC ETH BCH LTC EOS].freeze
+
   def prices
+    @coins_prices = CurrecyPriceService.instance.coin_price currencies: COINS
   end
 end
--- spec/controllers/crypto_currencies_controller_spec.rb
+++ spec/controllers/crypto_currencies_controller_spec.rb
@@ -2,7 +2,11 @@ require "rails_helper"
 
 RSpec.describe CryptoCurrenciesController, type: :controller do
   describe "GET #prices" do
-    subject(:get_prices) { get :prices }
+    subject(:get_prices) {
+      VCR.use_cassette "crypto_compare/main_coins_prices" do
+        get :prices
+      end
+    }
 
     it "should return 200 OK" do
       get_prices
@@ -11,8 +15,13 @@ RSpec.describe CryptoCurrenciesController, type: :controller do
 
     it "should invoke the currency price service" do
       expect_any_instance_of(CurrecyPriceService).to receive(:coin_price)
-        .with(currencies: ["BTC", "ETH", "BCH", "LTC"])
+        .with(currencies: ["BTC", "ETH", "BCH", "LTC", "EOS"])
       get_prices
+    end
+
+    it "should have an currencies instance variable" do
+      get_prices
+      expect(assings(:coins_prices)).to_not be_empty
     end
   end
 end
