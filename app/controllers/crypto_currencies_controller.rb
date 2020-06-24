04bbcc882d1c860c2559eb8bcfc0a097b76c2f83
Mateus Luiz <mateuslfreitasb@gmail.com>
2020-06-24 16:44:11 -0300

    Dummy commit

--- app/controllers/crypto_currencies_controller.rb
+++ app/controllers/crypto_currencies_controller.rb
@@ -2,6 +2,12 @@ class CryptoCurrenciesController < ApplicationController
   COINS = %w[BTC ETH BCH LTC EOS].freeze
 
   def prices
+    set_coins
+  end
+
+  private
+
+  def set_coins
     @coins_prices = CurrecyPriceService.instance.coin_price currencies: COINS
   end
 end
